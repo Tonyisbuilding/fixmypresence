f2a0b223682333a1072e4b34708220d8e84d7530
Tonyisbuilding <anthonyadewuyi01@gmail.com>
2025-09-19 14:57:14 +0100

    ade

--- components/sections/nav-bar.tsx
+++ components/sections/nav-bar.tsx
@@ -64,7 +64,7 @@ const Navbar: React.FC<NavbarProps> = ({ onPriorityCallClick, links = DEFAULT_LI
           {/* logo (left) */}
           <Link href="/" className="homepage-logo" aria-label="FixMyPresence Home">
             <Image
-              src="/images/Fixmypresence-logo.svg"
+              src="/images/FMP logo.png"
               alt="FixMyPresence Logo"
               className="homepage-logo-img desktop-only"
               width={360}
